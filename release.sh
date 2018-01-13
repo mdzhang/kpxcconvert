bf7c8ea110a2403c431ef934516ab787e669ae38
Michelle D Zhang <zhang.michelle.d@gmail.com>
2018-01-13 11:44:48 -0500

    [ci] Release message formatting

--- release.sh
+++ release.sh
@@ -6,12 +6,12 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 LAST_VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
 
-DESCRIPTION="Bump ${LAST_VERSION} → ${VERSION}"
+DESCRIPTION="# Bump ${LAST_VERSION} → ${VERSION}"
 
 if [ "${VERSION}" != "${LAST_VERSION}" ]; then
   DESCRIPTION="${DESCRIPTION}<br/>**Changelog**<br/>$(git log ${LAST_VERSION}..HEAD --oneline --no-merges)"
 else
-  DESCRIPTION="Bump N/A → ${VERSION}<br/>**Changelog**<br/>$(git log ${VERSION} --oneline --no-merges)"
+  DESCRIPTION="# Bump N/A → ${VERSION}<br/>**Changelog**<br/>$(git log ${VERSION} --oneline --no-merges)"
 fi
 
 github-release mdzhang/kpxcconvert "${VERSION}" "${BRANCH}" "${DESCRIPTION}" "dist/kpxcconvert-${VERSION}-*"
